f4ba160ef228421185bd76f363d474bfceab485d
shane86920915 <73564809+shane86920915@users.noreply.github.com>
2021-08-25 11:53:13 -0400

    Created Employee Creat and View Functionality

--- EmployeesTeamConsole/ProjectUI.cs
+++ EmployeesTeamConsole/ProjectUI.cs
@@ -7,15 +7,15 @@ using System.Threading.Tasks;
 
 namespace EmployeesTeamConsole
 {
-     class ProjectUI
+    class ProjectUI
     {
-        private readonly Employee _employee = new Employee();
+        private readonly EmployeeRepo _employee = new EmployeeRepo();
 
         public void Run()
         {
-
+            Menu();
         }
-        
+
         public void Menu()
         {
             var keepRunning = true;
@@ -28,7 +28,7 @@ namespace EmployeesTeamConsole
                     "3. Search for an individual employee\n" +
                     "4. Update an existing employee\n" +
                     "5. Delete an employee profile\n" +
-                    "6. Exit" );
+                    "6. Exit");
 
                 var input = Console.ReadLine();
 
@@ -57,17 +57,54 @@ namespace EmployeesTeamConsole
                         Console.WriteLine("Invalid input: select from one of the menu options!");
                         break;
                 }
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
 
             }
 
         }
         private void CreateEmployee()
-            {
+        {
+            Employee newEmployee = new Employee();
 
-            }
+            Console.WriteLine("Create an ID uniquely for the new employee:");
+            string inputAsString = Console.ReadLine();
+            int inputAsInt = int.Parse(inputAsString);
+            newEmployee.EmpId = inputAsInt;
+
+            Console.WriteLine("Enter the first name:");
+            newEmployee.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter the last name:");
+            newEmployee.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter the address:");
+            newEmployee.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter the city:");
+            newEmployee.City = Console.ReadLine();
+
+            Console.WriteLine("enter the state:");
+            newEmployee.State = Console.ReadLine();
+
+            Console.WriteLine("enter a phone number:");
+            newEmployee.PhoneNumber = Console.ReadLine();
+
+            _employee.CreateEmployee(newEmployee);
+        }
         private void ViewAllEmployees()
         {
 
+            List<Employee> employeesList = _employee.GetEmployee();
+
+            foreach (var employee in employeesList)
+            {
+                Console.WriteLine($"Employee ID: {employee.EmpId}\n" +
+                    $"First Name: {employee.FirstName}\n" +
+                    $"Last Name: {employee.LastName}");
+               
+            }
         }
 
         private void SearchForEmployee()
